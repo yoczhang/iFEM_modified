e14e5ffb999e2bb80b2b23a9495a2a39aa3939b7
yczhang <yoczhang@126.com>
2019-11-19 21:28:27 +0800

    up

--- ProjectMAC/MAC_SaddlePoint/as_gmres_afunc.m
+++ ProjectMAC/MAC_SaddlePoint/as_gmres_afunc.m
@@ -1,4 +1,4 @@
-function as_gmres_afunc(U)
+function r = as_gmres_afunc(U)
 %- The input U contains uh, vh and ph
 
 %- Firstly, we need to get the size of uh, vh and ph
@@ -20,6 +20,28 @@ ph = U(location_ph);
 ph = reshape(ph, n, n);
 
 
+%- The following is to compute A*x
+ru = zeros(n,n+1); 
+rv = zeros(n+1,n); 
+rp = zeros(n,n);
 
+i = 2:n-1; 
+j = 2:n;
+ru(i,j) = (ph(i,j) - ph(i,j-1))/h + gamma*uh(i,j) - mu*(uh(i-1,j) + uh(i+1,j) + uh(i,j-1) + uh(i,j+1) - 4*uh(i,j))/h^2;
+ru(1,j) = (ph(1,j) - ph(1,j-1))/h + gamma*uh(1,j) - mu*(2*uTop(1,j) + uh(2,j) + uh(1,j-1) + uh(1,j+1) - 5*uh(1,j))/h^2;
+ru(n,j) = (ph(n,j) - ph(n,j-1))/h + gamma*uh(n,j) - mu*(uh(n-1,j) + 2*uBot(1,j) + uh(n,j-1) + uh(n,j+1) - 5*uh(n,j))/h^2;
+
+i = 2:n; 
+j = 2:n-1;
+rv(i,j) = (ph(i-1,j) - ph(i,j))/h + gamma*vh(i,j) - mu*(vh(i-1,j) + vh(i+1,j) + vh(i,j-1) + vh(i,j+1) - 4*vh(i,j))/h^2;
+rv(i,1) = (ph(i-1,1) - ph(i,1))/h + gamma*vh(i,1) - mu*(vh(i-1,1) + vh(i+1,1) + 2*vLef(i,1) + vh(i,2) - 5*vh(i,1))/h^2;
+rv(i,n) = (ph(i-1,n) - ph(i,n))/h + gamma*vh(i,n) - mu*(vh(i-1,n) + vh(i+1,n) + vh(i,n-1) + 2*vRig(i,1) - 5*vh(i,n))/h^2;
+    
+i = 1:n; j = 1:n;
+rp(i,j) = - ph(i,j) - (uh(i,j+1) - uh(i,j))/h + (vh(i,j) - vh(i+1,j))/h;
+
+
+%- the final results
+r = [ru(:); rv(:); rp(:)];
 
 end
--- ProjectMAC/MAC_SaddlePoint/test_using_gmres.m
+++ ProjectMAC/MAC_SaddlePoint/test_using_gmres.m
@@ -1,20 +1,26 @@
 function test_using_gmres()
 
-n = 21;
+clear
+clc
+close all
+
+n = 11;
 b = afun(ones(n,1));
 tol = 1e-12;  maxit = 15; 
-x1 = gmres(@afun,b,10,tol,maxit);
+x1 = gmres(@afun,b,10,tol,maxit, @mfun);
 
 
     function y = afun(x)
-        y = [0; x(1:n-1)] + ...
-              [((n-1)/2:-1:0)'; (1:(n-1)/2)'].*x + ...
-              [x(2:n); 0];
-        y = 0*y;
+        nn = length(x);
+        y = [0; x(1:nn-1)] + ...
+              [((nn-1)/2:-1:0)'; (1:(nn-1)/2)'].*x + ...
+              [x(2:nn); 0];
+        %y = 0*y;
     end
 
     function y = mfun(r)
-        y = r ./ [((n-1)/2:-1:1)'; 1; (1:(n-1)/2)'];
+        nn = length(r);
+        y = r ./ [((nn-1)/2:-1:1)'; 1; (1:(nn-1)/2)'];
     end
 
 end
